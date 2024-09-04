ddfde995edfec4e9ef3f03af29e6cf2a38cec626
lokesh <lokeshambati@123>
2024-09-04 09:19:53 +0530

    updated Terraform code

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "myterraformbucket180900"
-    key    = "backend/ToDo-App.tfstate"
-    region = "us-east-1"
+    bucket         = "myterraformbucket180900"
+    key            = "backend/ToDo-App.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "mydynamodbtable"
   }
 }
